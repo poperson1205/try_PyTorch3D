5189c1a612155c8609a3b466bca6d1cc0436a24f
Seungki Kim <poperson1205@gmail.com>
2020-12-31 18:31:11 +0900

    Add tutorial notebook files

---
+++ download_tutorials.sh
@@ -0,0 +1,9 @@
+mkdir tutorials && cd tutorials
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/bundle_adjustment.ipynb
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/camera_position_optimization_with_differentiable_rendering.ipynb
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/dataloaders_ShapeNetCore_R2N2.ipynb
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/deform_source_mesh_to_target_mesh.ipynb
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/fit_textured_mesh.ipynb
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/render_colored_points.ipynb
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/render_densepose.ipynb
+wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/render_textured_meshes.ipynb
